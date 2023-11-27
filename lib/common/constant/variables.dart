280bde85fff2a1f9b056feb8b3853a4085399bf0
rodiyatun <mardiyahrodiyatun39@gmail.com>
2023-11-27 15:51:09 +0700

    link update

--- lib/common/constant/variables.dart
+++ lib/common/constant/variables.dart
@@ -1,4 +1,4 @@
 class Variables{
-  static const String baseUrl ='https://db90-36-73-32-46.ngrok-free.app';
+  static const String baseUrl ='https://3172-146-190-93-146.ngrok-free.app';
   static const String rajaongkirKey = 'a9da3c4359fafde97f03ee2be60147b2';
 }
